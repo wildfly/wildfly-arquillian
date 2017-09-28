b8e35a2bf1d51a6fdf16c566716e1a92476b149f
Dy, Markus <markusdy@users.noreply.github.com>
2017-09-28 09:14:36 +0200

    Split parameter at unquoted whitespaces

---
+++ common/src/main/java/org/jboss/as/arquillian/container/ParameterUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.arquillian.container;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Utility methods related to parsing parameters.
+ */
+public class ParameterUtils {
+
+    private static final Pattern WHITESPACE_IF_NOT_QUOTED = Pattern.compile("\"([^\"]*)\"|(\\S+)");
+
+    /**
+     * Splits the given string at quotes oder, if unquoted, at whitespaces.
+     *
+     * @param parameterString
+     * @return
+     */
+    public static ArrayList<String> splitParams(String parameterString) {
+
+        ArrayList<String> params = new ArrayList<>();
+        Matcher m = WHITESPACE_IF_NOT_QUOTED.matcher(parameterString);
+        while (m.find()) {
+            if (m.group(1) != null) {
+                // Add double-quoted string without the quotes
+                params.add(m.group(1));
+            } else {
+                // Add unquoted word
+                params.add(m.group(2));
+            }
+        }
+        return params;
+    }
+
+    private ParameterUtils() {
+        // Do not initialize this class
+    }
+}

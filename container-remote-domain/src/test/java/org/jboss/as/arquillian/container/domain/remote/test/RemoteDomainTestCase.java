672b5ce5d885a7e4666d49be7899d223e3b73437
James R. Perkins <jperkins@redhat.com>
2025-01-03 16:58:10 -0800

    [WFARQ-149] Migrate the container-remote-domain module to test with JUnit 5.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- container-remote-domain/src/test/java/org/jboss/as/arquillian/container/domain/remote/test/RemoteDomainTestCase.java
+++ container-remote-domain/src/test/java/org/jboss/as/arquillian/container/domain/remote/test/RemoteDomainTestCase.java
@@ -8,14 +8,16 @@ import org.jboss.arquillian.container.test.api.ContainerController;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.TargetsContainer;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * For Domain server DeployableContianer implementations, the DeployableContainer will register
@@ -23,7 +25,8 @@ import org.junit.runner.RunWith;
  *
  * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
  */
-@RunWith(Arquillian.class)
+@ExtendWith(ArquillianExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RemoteDomainTestCase {
 
     @ArquillianResource
@@ -36,19 +39,19 @@ public class RemoteDomainTestCase {
     }
 
     @Test
-    @InSequence(1)
+    @Order(1)
     @OperateOnDeployment("dep1")
     @TargetsContainer("master:server-one")
     public void shouldRunInContainer1() throws Exception {
-        Assert.assertTrue(controller.isStarted("master:server-one"));
+        Assertions.assertTrue(controller.isStarted("master:server-one"));
         System.out.println("in..container");
     }
 
     @Test
-    @InSequence(2)
+    @Order(2)
     @OperateOnDeployment("dep1")
     @TargetsContainer("master:server-two")
     public void shouldRunInContainer2() throws Exception {
-        Assert.assertTrue(controller.isStarted("master:server-two"));
+        Assertions.assertTrue(controller.isStarted("master:server-two"));
     }
 }

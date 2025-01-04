93d8841b8f453b721d3eb50f7c6a42cec113ad7d
James R. Perkins <jperkins@redhat.com>
2025-01-03 16:53:22 -0800

    [WFARQ-149] Migrate the container-managed-domain module to test with JUnit 5.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- container-managed-domain/src/test/java/org/jboss/as/arquillian/container/domain/managed/test/ClientDeploymentTestCase.java
+++ container-managed-domain/src/test/java/org/jboss/as/arquillian/container/domain/managed/test/ClientDeploymentTestCase.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.container.domain.ArchiveDeployer;
 import org.jboss.as.arquillian.container.domain.ManagementClient;
@@ -17,9 +17,9 @@ import org.jboss.dmr.ModelNode;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.wildfly.arquillian.domain.api.TargetsServerGroup;
 
 /**
@@ -27,7 +27,7 @@ import org.wildfly.arquillian.domain.api.TargetsServerGroup;
  *
  * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
  */
-@RunWith(Arquillian.class)
+@ExtendWith(ArquillianExtension.class)
 @RunAsClient
 public class ClientDeploymentTestCase {
 
@@ -42,36 +42,36 @@ public class ClientDeploymentTestCase {
 
     @Test
     public void globalDeploymentTest(@ArquillianResource ManagementClient client) throws Exception {
-        Assert.assertNotNull("Expected the ArchiveDeployer to be injected", deployer);
+        Assertions.assertNotNull(deployer, "Expected the ArchiveDeployer to be injected");
         testDeploy(deployer, client);
     }
 
     @Test
     public void parameterDeploymentTest(@ArquillianResource ManagementClient client,
             @ArquillianResource ArchiveDeployer deployer) throws Exception {
-        Assert.assertNotNull("Expected the ArchiveDeployer to be injected", deployer);
+        Assertions.assertNotNull(deployer, "Expected the ArchiveDeployer to be injected");
         testDeploy(deployer, client);
     }
 
     private void testDeploy(final ArchiveDeployer deployer, final ManagementClient client) throws Exception {
         final WebArchive deployment = createDeployment("test-client-deployment.war");
-        Assert.assertEquals(deployment.getName(), deployer.deploy(deployment, Collections.singleton("main-server-group")));
+        Assertions.assertEquals(deployment.getName(), deployer.deploy(deployment, Collections.singleton("main-server-group")));
 
         // Check the server for the deployment
         final ModelNode address = Operations.createAddress("deployment", deployment.getName());
         ModelNode result = client.getControllerClient().execute(Operations.createReadAttributeOperation(address, "name"));
         if (!Operations.isSuccessfulOutcome(result)) {
-            Assert.fail(Operations.getFailureDescription(result).asString());
+            Assertions.fail(Operations.getFailureDescription(result).asString());
         }
-        Assert.assertEquals(deployment.getName(), Operations.readResult(result).asString());
+        Assertions.assertEquals(deployment.getName(), Operations.readResult(result).asString());
 
         final ModelNode serverGroupAddress = Operations.createAddress("server-group", "main-server-group", "deployment",
                 deployment.getName());
         result = client.getControllerClient().execute(Operations.createReadAttributeOperation(serverGroupAddress, "enabled"));
         if (!Operations.isSuccessfulOutcome(result)) {
-            Assert.fail(Operations.getFailureDescription(result).asString());
+            Assertions.fail(Operations.getFailureDescription(result).asString());
         }
-        Assert.assertTrue("The deployment was expected to be enabled", Operations.readResult(result).asBoolean());
+        Assertions.assertTrue(Operations.readResult(result).asBoolean(), "The deployment was expected to be enabled");
         deployer.undeploy(deployment.getName(), "main-server-group");
     }
 

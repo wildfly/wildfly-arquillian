f932833d39c3f3aebdfb71a8678fad1dce66724d
James R. Perkins <jperkins@redhat.com>
2025-01-03 16:53:22 -0800

    [WFARQ-149] Migrate the container-managed-domain module to test with JUnit 5.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- container-managed-domain/src/test/java/org/jboss/as/arquillian/container/domain/managed/test/ManagedDomainTestCase.java
+++ container-managed-domain/src/test/java/org/jboss/as/arquillian/container/domain/managed/test/ManagedDomainTestCase.java
@@ -7,12 +7,12 @@ package org.jboss.as.arquillian.container.domain.managed.test;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.TargetsContainer;
-import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.wildfly.arquillian.domain.api.TargetsServerGroup;
 
 /**
@@ -21,7 +21,7 @@ import org.wildfly.arquillian.domain.api.TargetsServerGroup;
  *
  * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
  */
-@RunWith(Arquillian.class)
+@ExtendWith(ArquillianExtension.class)
 public class ManagedDomainTestCase {
 
     @Deployment(name = "dep1")
@@ -36,8 +36,8 @@ public class ManagedDomainTestCase {
     public void shouldRunInContainer1() throws Exception {
         // Get the logger path which should contain the name of the server
         final String logDir = System.getProperty("jboss.server.log.dir");
-        Assert.assertNotNull("Could not determine the jboss.server.log.dir property", logDir);
-        Assert.assertTrue("Log dir should contain server-one: " + logDir, logDir.contains("server-one"));
+        Assertions.assertNotNull(logDir, "Could not determine the jboss.server.log.dir property");
+        Assertions.assertTrue(logDir.contains("server-one"), "Log dir should contain server-one: " + logDir);
     }
 
     @Test
@@ -45,7 +45,7 @@ public class ManagedDomainTestCase {
     public void shouldRunInContainer2() throws Exception {
         // Get the logger path which should contain the name of the server
         final String logDir = System.getProperty("jboss.server.log.dir");
-        Assert.assertNotNull("Could not determine the jboss.server.log.dir property", logDir);
-        Assert.assertTrue("Log dir should contain server-two: " + logDir, logDir.contains("server-two"));
+        Assertions.assertNotNull(logDir, "Could not determine the jboss.server.log.dir property");
+        Assertions.assertTrue(logDir.contains("server-two"), "Log dir should contain server-two: " + logDir);
     }
 }
